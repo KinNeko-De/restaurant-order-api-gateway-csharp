644f6039ba6dea5677c05d6582a98bf2759a344f
Nils Diekmann <nils@diekmann-germany.de>
2023-02-19 14:20:05 +0100

    Api Gateway dummy written in C#

---
+++ Restaurant.Order.ApiGateway/Orders/OrderController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static Grpc.Core.Metadata;
+
+namespace Restaurant.Order.ApiGateway.Orders;
+
+[Route("[controller]")]
+[ApiController]
+public class OrderController : ControllerBase
+{
+    public struct OrderCreatedResponse {
+        public required Guid Id { get; set; }
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public ActionResult<OrderCreatedResponse> CreateOrder(CancellationToken cancellationToken)
+    {
+        var response = new OrderCreatedResponse()
+        {
+            Id = Guid.NewGuid()
+        };
+
+        return CreatedAtAction(nameof(GetOrder), new { orderId = response.Id }, response);
+    }
+
+    public struct OrderResponse
+    {
+        public required Guid Id { get; set; }
+        public required int Version { get; set; }
+    }
+
+    [HttpGet("{orderId:Guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult<OrderResponse> GetOrder(Guid orderId, CancellationToken cancellationToken)
+    {
+        var response = new OrderResponse()
+        {
+            Id = orderId,
+            Version = 1
+        };
+
+        return Ok(response);
+    }
+}
---
+++ tools/sut-up-background.cmd
@@ -0,0 +1,12 @@
+:: starts the system under test
+docker network create restaurant-dev-net
+
+pushd ..\restaurant.order.apigateway
+dotnet publish -o ../sut/publish -c Release --no-self-contained
+popd
+
+pushd ..\sut
+docker compose --verbose up --build --remove-orphans --wait
+popd
+
+pause
